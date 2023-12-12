c4704bb01b371f06a6836475a3f1193dee483836
Aayush9029 <developer@aayush.art>
2023-12-12 08:45:27 -0500

    updated few files

---
+++ src/pages/privacy.tsx
@@ -0,0 +1,30 @@
+
+
+export default function PrivacyPage() {
+    return (
+        <>
+            <p>
+                Our Privacy Policy
+            </p>
+            <p>
+                <b>Overview</b>
+                <br />
+                The app is designed with your right to privacy in mind. The app does not
+                explicitly collect any usage or analytics information. None of your data
+                in The app leaves your device.
+            </p>
+            <p>
+                <b>Your Data in The app</b>
+                <br />I have no access to any of your data. device.
+            </p>
+            <p>
+                <b>Analytics</b>
+                <br />
+                The app does not explicitly collect any usage or analytics information.
+                Depending on your device’s privacy settings, Apple may collect and share
+                analytics information — such as crash logs, battery usage.
+            </p>
+            <p>*For legal reasons: These are subject to change.</p>
+        </>
+    );
+}
